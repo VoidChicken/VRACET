7da78223be71a3c3db611aee7996e376addbbcc2
Void Chicken <VoidChicken@users.noreply.github.com>
2017-09-17 03:05:14 -0700

    Fixed again

--- VRACET.lua
+++ VRACET.lua
@@ -1,10 +1,11 @@
 return function(command, params)
     if command == "music" then
       if game.Soundscape.Music then
-        game.Soudscape.Music.SoundId = "rbxassetid://" .. params
+        game.Soundscape.Music.SoundId = "rbxassetid://" .. params
        else
          local sound = Instance.new("Sound")
           sound.Parent = game.Soundscape
+          sound.Name = "Music"
           game.Soundscape.Music.SoundId = "rbxassetid://" .. params
       end
     end
