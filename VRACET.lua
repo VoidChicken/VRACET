ebed960f42d35a3b5c1a6d7df7579d0385a7dbc8
Void Chicken <VoidChicken@users.noreply.github.com>
2017-09-17 02:46:14 -0700

    Create VRACET.lua

---
+++ VRACET.lua
@@ -0,0 +1,12 @@
+return function(command, params)
+    if command == "music" then
+      if game.Soundscape.Music then
+        game.Soudscape.Music.SoundId = "rbxassetid://" .. params
+       else
+         local sound = Instance.new("Sound")
+          sound.Parent = game.Soundscape
+          game.Soudscape.Music.SoundId = "rbxassetid://" .. params
+      end
+
+  return false
+end
